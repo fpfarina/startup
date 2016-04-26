4d1a7f78369214108be34182468a5c8e74eef8f4
Fernando Farina <fpfarina@hotmail.com>
2016-04-26 10:04:20 -0300

    Topic1: optimizations, better code and comments added.

--- 01-JS-DOM-APIs/js/index.js
+++ 01-JS-DOM-APIs/js/index.js
@@ -1,50 +1,55 @@
 'use strict';
 
-/* The global variable firstClick advices if the user has pressed the button for first time. This variable
-*  its used, because the first joke will be shown by the EX 6, while the next jokes will be shown by the
-*  AJAX Call of the EX 7.
-*  firstClick = 0; The user hasn't pressed the button yet.
-*  firstClick > 0; Tue user has pressed the button for first time.
- */
-var firstClick = 0;
+/* The global variable Clicks advices how many times the user has pressed the joke button. Tt's used in EX 6, to
+ * control the Time  Out error. */
+var Clicks = 0;
 
-/* The global variable JSuccess it's used at exercise 6, to determinate with the timer if the server has answered.
-*/
+/* The global variable JSuccess it's used at exercise 6, to determinate if the server has answered and avoid
+ * showing old "time out" errors. */
 var JSuccess = false;
 
-/* This function removes all child nodes of a given node.
-* Just looks for all the childs and delete one by one.*/
+/* It will clear all the content of a give node.
+*  Method: Just looks for all childNodes of a given node and delete one by one.*/
 function removeAllChilds(node){
     while ( node.hasChildNodes() ) {
         node.removeChild(node.firstChild);
     }
 }
 
-/* EX 6 - This function uses XMLHttpRequest, to get a joke from the given source
-* This function it's really basic, in the EX 1.7 i've make a better implementation.
-* */
+/* EX 6 - This function uses XMLHttpRequest, to get a joke from the given source.
+ * This function it's really basic, in the EX 1.7 i've make a better implementation.
+ * It can't determinate what kind of error it's happening, only send a "Time out error".
+ * Any new click on the button will kill the last call. To make this happen, i use the global variable Clicks.
+ * If the callId is equal to Click, tue function will show the joke / error because it's match with the last
+ * click, but if not, it'll do nothing. I know it's not efficient.*/
 function tellMeAJoke(){
+    JSuccess = false; ///* Set the global variable JSuccess to false. Tt's used
+    // to determinate if the server has answered and avoid showing old "time out" errors.
+    // If JSuccess = true, the server has answered. If not, the time out error, will be call.
+    var callId = Clicks;
     var req = new XMLHttpRequest(); //A new HttpRequest object
     req.open ("GET", "http://api.icndb.com/jokes/random", true); //Configuration: GET - src - Async
     setTimeout(function(){
-        if (firstClick < 2 && JSuccess == false){
+        if (Clicks == callId && JSuccess == false){ //Is it the last call and now answer yet?
             changeColor("red");
             changeAdvice("ERROR: Time OUT!!");
         }
-    },4000);
+    },3000);
     req.addEventListener("load", function(){ // When the server answer --> show the joke!!.
-        showJoke(req); // This function it's the one that show JOKES. It's used by both 1.6 and 1.7 exercises.
-        changeColor("green");
-        changeAdvice("SUCCESS!!");
-        JSuccess = true;
+        if (Clicks == callId){ //Is it the last call?
+            showJoke(req); // This function it's the one that show JOKES. It's used by exercises 6 and 7.
+            changeColor("green");
+            changeAdvice("SUCCESS!!");
+            JSuccess = true;
+        }
     });
     req.send(null); // send the HttpRequest.
 }
 
 /* EX 7 (KEY) and 6
-* function showJoke(reqJSON)
-* where req -> It's a JSON object
-*  This function takes a JSON answer of the HttpRequest, and show it in the HTML Document, modifying the DOM.
+ * function showJoke(reqJSON)
+ * where reqJSON -> It's a JSON object
+ *  This function takes a JSON answer of the HttpRequest, and show it in the HTML Document, modifying the DOM.
 */
 function showJoke (reqJSON){
     if (reqJSON.status < 400){ // if < 400 ->> Success!
@@ -90,8 +95,8 @@ function CONFIG() {
         }
         return text;
     }
-    this.info = function(){ //This method show in the console a description of the config object showing all the values.
-                            //Set hidePassword = false; to show the password
+    this.info = function(){ //This method show in the console a description of the config object, showing all values.
+                            //Set hidePassword = false; show the password.
         console.log("CONFIG AJAX CALL: " + "method: " + this.action + " url: " + this.url + " async: " +
             this.async + " user: " + this.user + " password: " + writePassword(this.hidePassword, this.password) +
             " SEND:" + this.send);
@@ -99,8 +104,9 @@ function CONFIG() {
 }
 
 /* EX 7 (KEY)
-*  This function prepare the AJAXCall to get a joke, creating the config object, setting all
-*  the parameters, and calling the AJAXCall.
+ *  This function prepare an AJAXCall to get a joke:
+ *  1- Create the config object and set all the parameters.
+ *  2- Send the AJAXCall.
  */
 function prepareAJAXCall(){
     var config = new CONFIG(); //Creating the config object.
@@ -110,16 +116,16 @@ function prepareAJAXCall(){
     config.hidePassword = false; //my password will be not a secret if you are looking the console
     config.url = "http://api.icndb.com/jokes/random"; //src
     config.info(); //print the object in the console, just for debugging
-    AJAXCall(config).then(function(req) {
+    AJAXCall(config).then(function(req) { //Success function to promise
         changeColor("green");
         changeAdvice("SUCCESS!!");
-        showJoke(req);
-    }, function(error) {
+        showJoke(req); //Show Joke
+    }, function(error) { //Error function to promise
         var txt = "";
         txt = error.toString().slice(7);
         var first = txt[0];
         console.log("!AJAXCall error >> " + txt);
-        changeAdvice("ERROR: " + txt.slice(1));
+        changeAdvice("ERROR: " + txt.slice(1)); //Show Error
         switch (first) {
             case "1":
             case "2":
@@ -135,7 +141,7 @@ function prepareAJAXCall(){
     });
 }
 
-/* EX 9 */
+/* EX 9 - this function change the color of the joke section, to the given color. */
 function changeColor(color){
     document.getElementById("joke").parentNode.style.background = "lightgray";
     setTimeout(function(){
@@ -143,39 +149,40 @@ function changeColor(color){
     }, 250);
 }
 
+/* Change the advice input text - Here will be notified error or success messages. */
 function changeAdvice(txt){
     document.getElementById("advice").value = txt;
 }
 
 
-/* EX 1.7 (KEY)
+/* 7 (KEY)
 *  This function makes an AJAX Call.
-*  AJAXCall (config, callback) where config is a CONFIG object and callback is a function()
+*  It waits as argument a CONFIG object.
  */
 
 function AJAXCall(config) {
     return new Promise(function (succeed, fail) {
         var actions = ["GET", "POST", "PUT", "DELETE"];
-        if (actions.indexOf(config.action) == -1) {
+        if (actions.indexOf(config.action) == -1) { //Bad input
             fail(new Error("1" + config.action + " is not a valid method. Please select one of this: " +
                 "GET, POST, PUT or DELETE."));
             return;
         }
-        if (typeof(config.async) != "boolean") {
+        if (typeof(config.async) != "boolean") { //Bad input
             fail(new Error("2config.async must be BOOLEAN."));
             return;
         }
         var req = new XMLHttpRequest();
         req.open(config.action, config.url, config.async, config.user, config.password);
         req.addEventListener("load", function () {
-            if (req.status < 400) {
-                succeed(req);
+            if (req.status < 400) { //Success?
+                succeed(req); //Yes!
             }
             else
-                fail(new Error("3"+req.statusText));
+                fail(new Error("3"+req.statusText)); //No!
         });
         req.addEventListener("error", function () {
-            fail(new Error("4Network error."));
+            fail(new Error("4Network error.")); //Network error.
         });
         req.send(config.send);
     });
@@ -183,26 +190,22 @@ function AJAXCall(config) {
 
 // Before doing anything, we want the DOMContentLoaded.
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('hide').style.display="block"; //EX 4 (KEY) - Hello world fade in.
+    /*EX 4 (KEY) - Hello world fade in. The animation is made by CSS.*/
+    document.getElementById('hide').style.display="block";
     setTimeout(function(){
         document.getElementById('hide').style.opacity=1;} //FADE IN
         /*I don't know accurately why, but without the timer, this function: change the visibility to 1, but don't make the CSS
          * animation. Strangely, with a 1ms timer, it's change the visibility to 1 and MAKE the CSS animation. I've seen that
-         * it's a common reported issue. I think that in the moment of the DOMContentLoaded, the browser assign the new css rule,
-         * like an "initial style property" and because of that don't make the animation.
+         * it's a common reported issue.
          * */
     , 1);
 
-    document.getElementById("joke").addEventListener("click", function(){ //When the user click the "joke" button
-        if (firstClick > 0) { //if not is the first click -> EX 7
-            prepareAJAXCall();
-        }
-        else
-        {
-            tellMeAJoke(); //if is the first click -> EX 6
-
-        }
-        firstClick =+ 1;
+    document.getElementById("joke").addEventListener("click", function(){ //When the user click the "joke" button.
+        Clicks += 1;
+        var option = document.getElementsByName("ex"); //Check what radio button it's checked.
+            if (option[0].checked) // EX 7
+                prepareAJAXCall();
+            else
+                tellMeAJoke(); // EX 6
     });
 });
-
