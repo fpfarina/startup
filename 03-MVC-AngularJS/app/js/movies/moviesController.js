6b39f2ca962cd4356bb892d8eec7866a2a10d648
Fernando Farina <fpfarina@hotmail.com>
2016-05-05 17:04:49 -0300

    Topip 3: visual styling and validation features. This version it's not responsive.

--- 03-MVC-AngularJS/app/js/movies/moviesController.js
+++ 03-MVC-AngularJS/app/js/movies/moviesController.js
@@ -4,56 +4,65 @@ let moviesCtrl = angular.module('moviesCtrl', []);
 
 moviesCtrl.controller('MoviesCtrl', ['$scope', 'MoviesList', 'MoviesStorage', function($scope, MoviesList, MoviesStorage) {
 
-    let moviesList = new MoviesList; // First i make my moviesList
-    moviesList = MoviesStorage.data.movies; // Add all the movies to the list.
+    let moviesList = new MoviesList; // First i make an empty moviesList
+    moviesList = MoviesStorage.data.movies; // Add all the movies to the list (by the service MoviesStorage).
 
     $scope.movies = moviesList.movies; // movies -> MoviesList.movies
-    $scope.select = MoviesStorage.data.select.id; // load from localStorage the select id
-    $scope.selectedMovie = $scope.movies[$scope.select]; // The selected movie.
+    $scope.select = MoviesStorage.data.select.id; // Loads from localStorage the last remembered index
+    $scope.selectedMovie = $scope.movies[$scope.select]; // Select the last index.
+    $scope.noMovies =  ($scope.movies.length == 0); // In case there are not films.
+    if ($scope.movies.length > 0)
+        $scope.menuOption = "Edit movie: ";
+    else
+        $scope.menuOption = "No movies ";
 
-    MoviesStorage.data.movies = moviesList.movies;
-    console.log(moviesList);
+    let noMovie = new MoviesList; // A visual trick: if there´s not Movies, show an empty movie!!
+    noMovie.addMovie("","","");
 
+    MoviesStorage.data.movies = moviesList.movies; // Live data storage.
 
     $scope.isEmpty = function(string, optString=""){ // if the string is empty return a '-' char
-        if (string == "")
+        if (string == "" || string == undefined)
             return "-";
         else
             return (string + optString);
     };
 
-    $scope.set = function(index){ // when select another film
-        $scope.select = index; // Change select id
-        $scope.selectedMovie = $scope.movies[$scope.select]; // Change select movie
-        MoviesStorage.data.select.id = $scope.select; //Change MoviesStorage
+    $scope.set = function(index){ // when a new film in the list it's selected
+        if (index < 0) 
+            if ($scope.movies.length == 0){
+                $scope.noMovies = true;
+                $scope.menuOption = "No Movies ";
+                $scope.selectedMovie = noMovie;
+                return;
+                }
+            else
+                index = 0;
+
+            $scope.noMovies = false;
+            $scope.menuOption = "Edit movie: ";
+            $scope.select = index; // Changes select id
+            $scope.selectedMovie = $scope.movies[$scope.select]; // Changes select movie
+            MoviesStorage.data.select.id = $scope.select; //Changes select.id in MoviesStorage
     };
 
-    let newMovie = function(){
+    let newMovie = function(){ //NEW MOVIE
         moviesList.addMovie("","","");
         $scope.set($scope.movies.length-1);
-        refresh ();
     };
 
-    let deleteMovie = function(movieIndex){
+    let deleteMovie = function(movieIndex){ //DELETE MOVIE
         moviesList.deleteMovie(movieIndex);
         $scope.set($scope.select-1); // Select another film
-        if (!($scope.select == 0)){
-            MoviesStorage.data.select.id = $scope.select; // Change MoviesStorage
-        }
-        refresh();
     };
 
-    let refresh = function () {
-
-    };
-
-    $scope.setMenu = function(indexBtn){ // 0 - Add button 1 - Delete button
+    $scope.buttonsHandler = function(indexBtn){ // Button handler - 0 -> (Add button) 1 -> Delete button
         if (indexBtn == 0){
             newMovie();
-            $scope.menuOption = "New Movie: "
+            $scope.menuOption = "New movie: "
         }
         else
-            if (confirm("Are you sure you wanna delete "+ $scope.selectedMovie.title + " !! It would be irreversible."))
+            if (confirm("Are you sure you want to delete ''"+ $scope.selectedMovie.title + "'' ? It would be irreversible."))
                 deleteMovie($scope.select);
     };
 
@@ -63,6 +72,10 @@ moviesCtrl.controller('MoviesCtrl', ['$scope', 'MoviesList', 'MoviesStorage', fu
 
     $scope.deleteActor = function (indexActor){
         $scope.selectedMovie.deleteActor(indexActor);
+    };
+
+    $scope.isOdd = function (index){
+        return !((index % 2) == 1);
     }
 
 }]);
